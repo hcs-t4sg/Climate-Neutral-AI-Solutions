41c32e1e41077a106e649b359544a685ee1b80ef
Eric Hwang <eric_hwang@college.harvard.edu>
2023-09-28 15:30:46 -0400

    starter project starter code

---
+++ helpers.py
@@ -0,0 +1,7 @@
+from secret import OPENAI_API_KEY # You will need to instantiate your own secret.py file
+                                  # with the OpenAI key I gave you in the starter project instructions
+
+# Eventually will become our function that will get the climate-netural solution using LangChain
+# For the starter project, implement your work here!
+def get_sol(query):
+  pass
