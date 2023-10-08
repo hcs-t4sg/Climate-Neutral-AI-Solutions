28df7d1c230e6630d59f63b2e955323a47571fdc
Dinesh Vasireddy <dineshvasireddy@college.harvard.edu>
2023-10-08 01:55:01 -0400

    final_commit
    Working Model and Q/A System

--- app.py
+++ app.py
@@ -1,60 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def index():
-#     return 'Hello, World!'
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-# from flask import Flask, request, jsonify
-# from helpers import get_sol
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return 'Hello, World!'
-
-# @app.route('/ask', methods=['POST'])
-# def ask():
-#     data = request.json
-#     query = data.get('query')
-#     if not query:
-#         return jsonify({"error": "No query provided"}), 400
-
-#     answer = get_sol(query)
-#     return jsonify({"answer": answer})
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-# from flask import Flask, request, jsonify
-# from helpers import get_sol
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return 'Hello, World!'
-
-# @app.route('/get_solution', methods=['GET'])
-# def get_solution():
-#     data = request.json
-#     if not data or 'question' not in data:
-#         return jsonify({"error": "No question provided."}), 400
-
-#     question = data['question']
-#     solution = get_sol(question)
-#     return jsonify({"answer": solution})
-
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5000)
-
 from flask import Flask, request, jsonify, render_template
 from helpers import get_sol
 
@@ -63,17 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('input_form.html')
-
-# @app.route('/get_solution', methods=['POST'])
-# def get_solution():
-#     question = request.form.get('question')  # Get question from the form submission
-    
-#     if not question:
-#         return render_template('result.html', answer="Please submit a valid question.")
-    
-#     # Use the question as you did before
-#     answer = get_sol(question)
-#     return render_template('result.html', answer=answer["answer"])
 
 @app.route('/get_solution', methods=['POST'])
 def get_solution():
@@ -85,7 +17,7 @@ def get_solution():
     
     # Use the question as you did before
     answer = get_sol(question)
-    return render_template('result.html', answer=answer["answer"])
+    return render_template('result.html', answer=answer)
 
 
 if __name__ == "__main__":
--- helpers.py
+++ helpers.py
@@ -1,46 +1,10 @@
 from secret import OPENAI_API_KEY # You will need to instantiate your own secret.py file
                                   # with the OpenAI key I gave you in the starter project instructions
 
-# # Eventually will become our function that will get the climate-netural solution using LangChain
-# # For the starter project, implement your work here!
-#def get_sol(query):
-#  pass
-
-# from langchain.chains import RetrievalQA
-# from langchain.document_loaders import TextLoader
-# from langchain.embeddings.openai import OpenAIEmbeddings
-# from langchain.llms import OpenAI
-# from langchain.text_splitter import CharacterTextSplitter
-# from langchain.vectorstores import Chroma
-
-# def initialize_qa_system():
-#     # Load documents from source_docs
-#     loader = TextLoader("Climate-Neutral-.-Solutions/source_docs/climate_change_carbon_intensive_firms.pdf")
-#     documents = loader.load()
-
-#     # Tokenize the documents
-#     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-#     texts = text_splitter.split_documents(documents)
-
-#     # Create embeddings and doc search
-#     embeddings = OpenAIEmbeddings()
-#     docsearch = Chroma.from_documents(texts, embeddings)
-
-#     # Initialize the Question Answering system
-#     qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
-#     return qa
-
-# QA_SYSTEM = initialize_qa_system()
-
-# def get_sol(query):
-#     return QA_SYSTEM.run(query)
-
 import time
 
 from langchain.document_loaders import PDFMinerLoader
-# Assuming that there is a RetrievalQA system or similar in langchain
 from langchain.chains import RetrievalQA
-
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
@@ -49,61 +13,46 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 def initialize_qa_system():
-    # Load documents from the provided PDF
-    loader = PDFMinerLoader("source_docs/nature-based-solutions.pdf")
+    source_docs_arr = ["nature_based_solutions","climate_change_carbon_intensive_firms"]
 
-    pages = loader.load()
-    #time.sleep(20)  # Introducing a 20 seconds timeout
+    embeddings_list = []
 
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-    #time.sleep(20)  # Introducing a 20 seconds timeout
+    for source in source_docs_arr:
+        loader = PDFMinerLoader(f"source_docs/{source}.pdf")
+        pages = loader.load()
 
-    text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size = 1000,
-    chunk_overlap  = 20,
-    length_function = len,
-    add_start_index = True,
-    )
+        embeddings_instance = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
-    texts = text_splitter.split_text(str(pages))
+        # Instantiating Text Splitter
+        text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 2000,  
+        chunk_overlap  = 20,
+        length_function = len,
+        add_start_index = True,
+        )
 
-    x=0
-    arr=[]
+        # Splitting pdf content into text chunks
+        texts = text_splitter.split_text(str(pages))
 
-    while x < len(texts)-1:
-        if x+31>len(texts)-1:
-            short_texts = texts[x:(len(texts)-1)]
-            result = embeddings.embed_documents([short_texts])
-            arr.append(result)
-            x=x+32
-            time.sleep(20) 
-        else:
-            short_texts = texts[x:x+31]
-            result = embeddings.embed_documents([short_texts])
-            arr.append(result)
-            x=x+32
-            time.sleep(20) 
+        for text in texts:
+            embedding = embeddings_instance.embed_documents([text])[0]  
+            embeddings_list.append((text, embedding)) 
 
-    #result = embeddings.embed_documents([pages])
+    # Use these paired text-embedding tuples to create the FAISS index
+    faiss_index = FAISS.from_embeddings(embeddings_list, embeddings_instance)
 
-    faiss_index = FAISS.from_embeddings(arr,embeddings)
-    
-    #faiss_index = FAISS.from_documents(pages, embeddings)
-    #time.sleep(20)  # Introducing a 20 seconds timeout
-    
     llm = OpenAI(openai_api_key=OPENAI_API_KEY)
-    #time.sleep(20)  # Introducing a 20 seconds timeout
 
     # Initialize the QA system with the loaded documents
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=faiss_index.as_retriever())
-    
-    return qa
 
+    return qa
 
 def get_sol(question):
     QA_SYSTEM = initialize_qa_system()
-
-    # Query the QA system for an answer
-    answer = QA_SYSTEM.get_answer(question)
+    answer = QA_SYSTEM.run(question)
     return answer
+
+
+
+
