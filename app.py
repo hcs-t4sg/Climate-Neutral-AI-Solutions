48d9a32d39ce57f4ae181b43de1393a9d157c34f
paula1284 <112826629+paula1284@users.noreply.github.com>
2023-10-07 23:11:39 -0400

    version 1 of model, with prompt

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from helpers import get_sol
+import os
+from secret import OPENAI_API_KEY
 
 app = Flask(__name__)
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    query = {"company_type": "grocery", "target": "supply chain"}
+    answer = get_sol(query)
+    return f'{answer}'
 
 if __name__ == "__main__":
     app.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -1,7 +1,48 @@
-from secret import OPENAI_API_KEY # You will need to instantiate your own secret.py file
-                                  # with the OpenAI key I gave you in the starter project instructions
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import TextLoader, PyPDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain import PromptTemplate
+import os
 
 # Eventually will become our function that will get the climate-netural solution using LangChain
 # For the starter project, implement your work here!
-def get_sol(query):
-  pass
+def get_sol(query_params):
+  # loader = TextLoader("../../state_of_the_union.txt")
+  # documents = loader.load()
+
+  llm = OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"])
+
+  loader = PyPDFLoader("source_docs/Meta-2023-Path-to-Net-Zero.pdf")
+
+  text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap  = 20,
+    length_function = len,
+    is_separator_regex = False,
+  )
+
+  pages = loader.load_and_split(text_splitter=text_splitter)
+  texts = text_splitter.split_documents(pages)
+
+  embeddings = OpenAIEmbeddings()
+  docsearch = Chroma.from_documents(texts, embeddings)
+
+  qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever(), verbose=True)
+
+  template = """
+  We are a {company_type} company looking to cut our emissions through targeting our {target}.
+  
+  Give me three actionable, realistic to-dos.
+  """
+
+  prompt = PromptTemplate(
+      input_variables=["company_type","target"],
+      template=template,
+  )
+
+  final_prompt = prompt.format(company_type=query_params["company_type"], target=query_params["target"])
+
+  return qa.run(final_prompt)
