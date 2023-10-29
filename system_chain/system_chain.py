b7675f6e8958155a38eca1f3974096e9f993055a
Mark Pekala <mpekala@college.harvard.edu>
2023-10-29 15:59:01 -0400

    potentially working version

--- system_chain/system_chain.py
+++ system_chain/system_chain.py
@@ -3,51 +3,35 @@ from langchain.pydantic_v1 import BaseModel
 from langchain.chains import create_extraction_chain_pydantic
 from langchain.chat_models import ChatOpenAI
 import secret
-from scope_defns import SCOPE_DEFNS
-
-# Pydantic data class
-class CompanyProperties(BaseModel):
-    scope: list[Literal[tuple(SCOPE_DEFNS.keys())]]
-    description: str
-    goal_hint: str
-
-    @staticmethod
-    def new() -> "CompanyProperties":
-        return CompanyProperties(scope=[], description="", goal_hint="")
-
-    def __add__(self, other: "CompanyProperties") -> "CompanyProperties":
-        new_scope = list(set(self.scope).union(set(other.scope)))
-        new_description = f"{self.description} {other.description}"
-        new_goal_hint = f"{self.goal_hint} {other.goal_hint}"
-        return CompanyProperties(scope=new_scope, description=new_description, goal_hint=new_goal_hint)
-
-llm = ChatOpenAI(openai_api_key=secret.OPENAI_API_KEY, model="gpt-3.5-turbo", temperature=0.1)
-        
-# Extraction
-chain = create_extraction_chain_pydantic(pydantic_schema=CompanyProperties, llm=llm)
-
-# Run 
-inp = """
-We are a sports supply company concerned about scope 1, 2, and 3.7 emissions.
-
-We primarily make protective equipment (i.e. helmets, padding) for football and hockey.
-
-Our goal is to find better ways to source our materials.
-"""
-
-def clean_inp(inp_str: str):
-    """Removes newlines and annoying stuff so pydantic works properly"""
-    return inp_str.replace("\n", " ")
-
-def clean_outs(outs: list[CompanyProperties]):
-    """Combines multiple outputs into a single output"""
-    result = CompanyProperties.new()
-    for prop in outs:
-        result += prop
-    return result
+from scope_defns import get_company_scopes, fake_company, SCOPE_DEFNS
 
 
-inp = clean_inp(inp)
-outs = chain.run(input=inp, context="Only return a single Company Properties. Make the description and goal-hint human readable with proper punctation.")
-outs = clean_outs(outs)
-print(outs)
+def get_advice_prompt(copmany_description: str, specific: str):
+    scopes = get_company_scopes(copmany_description)
+    prompt_text = f"""
+    Your goal is to provide actionable insights for reducing specific scopes of carbon emissions.
+
+    The company describes themselves as follows:
+    {copmany_description}
+
+    They have given the following specific ideas:
+    {specific}
+
+    As a reminder, here are the explicit scopes relevant to this company:  
+    """
+    for scope in scopes:
+        prompt_text += f"\n{scope}: {SCOPE_DEFNS[scope]}\n"
+    prompt_text += """
+    Please provide, in markdown formatting, a summary of ways that this company could reduce their carbon emissions, being sure to cite the specific scopes whenever relevant.
+    """
+    return prompt_text
+
+
+llm = ChatOpenAI(
+    openai_api_key=secret.OPENAI_API_KEY, model="gpt-3.5-turbo", temperature=0.1
+)
+
+advice_prompt = get_advice_prompt(fake_company, "")
+text = llm.predict(advice_prompt)
+with open("Advice.md", "w") as fout:
+    fout.write(text)
